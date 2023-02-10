b76eafffa5c437d832f4b406a875df3070e9c8a5
Yury Kamenev <yurkam447@gmail.com>
2023-02-10 19:01:35 +0300

    Basic implementation example

--- src/main/kotlin/org/klogic/core/Term.kt
+++ src/main/kotlin/org/klogic/core/Term.kt
@@ -2,6 +2,8 @@ package org.klogic.core
 
 import org.klogic.core.RecursiveStream.Companion.nil
 import org.klogic.core.RecursiveStream.Companion.single
+import org.klogic.unify.UnificationState
+import kotlin.reflect.KClass
 
 /**
  * Represents a logic object.
@@ -101,3 +103,92 @@ fun Any.toTerm(): Term = when (this) {
     is Term -> this
     else -> error("Could not transform $this to term")
 }
+
+/*interface ITerm<T : Any> {
+    fun occurs(variable: MyVar<out Any>): Boolean
+
+    fun walk(substitution: Substitution): ITerm<T>
+
+    fun unify(other: ITerm<T>, unificationState: UnificationState): Goal {
+        val walkedThis = walk(unificationState.substitution)
+        val walkedOther = other.walk(unificationState.substitution)
+
+        return if (walkedOther is MyVar<*>) {
+            walkedOther.unifyImpl(walkedThis, unificationState)
+        } else {
+            walkedThis.unifyImpl(walkedOther, unificationState)
+        }
+    }
+
+    fun unifyImpl(walkedOther: ITerm<T>, unificationState: UnificationState): Goal
+}
+
+class MyVar<T : Any> @PublishedApi internal constructor(val index: Int, val variableType: KClass<T>) : ITerm<T> {
+    override fun occurs(variable: MyVar<out Any>): Boolean = this == variable
+
+    override fun walk(substitution: Substitution): MyVar<T> = TODO()
+
+    override fun unifyImpl(walkedOther: ITerm<T>, unificationState: UnificationState): Goal = TODO()
+}
+
+interface CustomTerm<T : CustomTerm<T>> : ITerm<T> {
+    fun unify(variable: MyVar<T>, unificationState: UnificationState): Goal =
+        variable.unify(this, unificationState)
+}
+
+
+class MySymbol(val name: String) : CustomTerm<MySymbol> {
+    override fun occurs(variable: MyVar<out Any>): Boolean = false
+
+    override fun walk(substitution: Substitution): ITerm<MySymbol> = this
+
+    override fun unifyImpl(walkedOther: ITerm<MySymbol>, unificationState: UnificationState): Goal {
+        if (this == walkedOther) TODO() else TODO()
+    }
+}
+
+class Pair(val first: MyVar<Any>, val second: ITerm<Any>) : CustomTerm<Pair> {
+    override fun occurs(variable: MyVar<out Any>): Boolean = first.occurs(variable) || second.occurs(variable)
+
+    override fun walk(substitution: Substitution): Pair = Pair(first.walk(substitution), second.walk(substitution))
+
+    override fun unifyImpl(walkedOther: ITerm<Pair>, unificationState: UnificationState): Goal {
+        val otherPair = walkedOther as Pair
+
+        first.unify(otherPair.first, unificationState)
+        second.unify(otherPair.second, unificationState)
+
+        TODO()
+    }
+}
+
+fun main() {
+    val unificationState = UnificationState.empty
+
+    val variableString1 = MyVar(1, String::class)
+
+    val variableInt1 = MyVar(3, Int::class)
+    val variableInt2 = MyVar(4, Int::class)
+    val variableSymbol = MyVar(5, MySymbol::class)
+
+    val symbol = MySymbol("a")
+    val pair1 = Pair(variableString1, symbol)
+
+    // Occurs
+    variableString1.occurs(variableString1)
+    variableString1.occurs(variableInt1)
+    symbol.occurs(variableInt1)
+    symbol.occurs(variableString1)
+
+    // Unify
+    variableInt1.unify(variableInt2, unificationState)
+    // variableInt1.unify(variableString1, unificationState) - does not compile
+    // variableInt1.unify(symbol, unificationState) - does not compile
+
+    // symbol.unify(variableInt1, unificationState) - does not compile
+    // symbol.unify(variableString1, unificationState) - does not compile
+    // symbol.unify(pair1, unificationState) - does not compile
+    symbol.unify(symbol, unificationState)
+    symbol.unify(variableSymbol, unificationState)
+    variableSymbol.unify(symbol, unificationState)
+}*/
