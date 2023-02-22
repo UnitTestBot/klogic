61d93d503d1a5c23d4f69a7b0d667390333e45af
Yury Kamenev <yurkam447@gmail.com>
2023-02-22 21:08:20 +0300

    Fixed review issue

--- klogic-core/src/main/kotlin/org/klogic/core/Substitution.kt
+++ klogic-core/src/main/kotlin/org/klogic/core/Substitution.kt
@@ -58,7 +58,9 @@ data class Substitution(private val innerSubstitution: PersistentMap<Var<*>, Ter
         (innerSubstitution.put(pair.first, pair.second)).toSubstitution()
 
     operator fun minus(other: Substitution): Substitution =
-        (innerSubstitution - other.innerSubstitution.keys).toSubstitution()
+        other.innerSubstitution.keys.fold(innerSubstitution) { acc, key ->
+            acc.remove(key)
+        }.toSubstitution()
 
     override fun toString(): String = innerSubstitution.toString()
 
