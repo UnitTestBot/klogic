efb9da4ea1f5eda6a31908ff3999cdcbdf00f613
Yury Kamenev <yurkam447@gmail.com>
2023-02-27 14:37:47 +0300

    Used ShadowJar with required dependencies as a main published artifact

--- klogic-core/build.gradle.kts
+++ klogic-core/build.gradle.kts
@@ -1,18 +1,32 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("org.klogic.klogic-base")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 dependencies{
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     testImplementation(project(":klogic-utils"))
     testImplementation(testFixtures(project(":klogic-utils")))
 }
 
+tasks.withType<ShadowJar> {
+    minimize()
+    // Replace the original jar with this executable jar
+    archiveClassifier.set("")
+
+    dependencies {
+        // Do not include kotlin-stdlib to the executable jar
+        exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
+        exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib-common"))
+    }
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
+            project.shadow.component(this)
             artifact(tasks["kotlinSourcesJar"])
         }
     }
