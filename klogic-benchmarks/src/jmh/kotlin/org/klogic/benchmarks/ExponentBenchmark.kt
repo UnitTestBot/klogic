2ff17d117556fe478f5be509954e9ea3ff7c5e5f
Yury Kamenev <yurkam447@gmail.com>
2023-06-06 10:00:29 +0300

    Removed ugly unification counter

--- klogic-benchmarks/src/jmh/kotlin/org/klogic/benchmarks/ExponentBenchmark.kt
+++ klogic-benchmarks/src/jmh/kotlin/org/klogic/benchmarks/ExponentBenchmark.kt
@@ -2,11 +2,10 @@ package org.klogic.benchmarks
 
 import org.klogic.core.Term
 import org.klogic.core.run
-import org.klogic.core.unificationCounter
 import org.klogic.utils.terms.OlegLogicNumber
 import org.klogic.utils.terms.OlegLogicNumber.Companion.toOlegLogicNumber
 import org.klogic.utils.terms.expᴼ
-import org.openjdk.jmh.annotations.*
+import org.openjdk.jmh.annotations.Benchmark
 import org.openjdk.jmh.infra.Blackhole
 
 open class ExponentBenchmark : AbstractKlogicBenchmark() {
@@ -16,6 +15,5 @@ open class ExponentBenchmark : AbstractKlogicBenchmark() {
         val power = 5u.toOlegLogicNumber()
 
         bh.consume(run(10, { result: Term<OlegLogicNumber> -> expᴼ(base, power, result) }))
-        println(unificationCounter)
     }
 }
--- klogic-core/src/main/kotlin/org/klogic/core/Term.kt
+++ klogic-core/src/main/kotlin/org/klogic/core/Term.kt
@@ -4,8 +4,6 @@ import org.klogic.core.RecursiveStream.Companion.nilStream
 import org.klogic.core.RecursiveStream.Companion.single
 import org.klogic.unify.UnificationState
 
-var unificationCounter: Int = 0
-
 /**
  * Represents a logic object. It has only one direct implementor - [Var], user terms have to implement [CustomTerm].
  *
@@ -85,7 +83,7 @@ sealed interface Term<T : Term<T>> {
     @Suppress("UNCHECKED_CAST")
     fun asReified(): T = this as T
 
-    infix fun `===`(other: Term<T>): Goal = (this unify other).also { unificationCounter++ }
+    infix fun `===`(other: Term<T>): Goal = this unify other
     infix fun `!==`(other: Term<T>): Goal = this ineq other
 
     fun isVar(): Boolean = this is Var<*>
