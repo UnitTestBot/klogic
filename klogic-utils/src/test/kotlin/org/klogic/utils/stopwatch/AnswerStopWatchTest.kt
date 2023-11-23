be4fc88b0c5522906470a71755cfa2efa63881ea
Yury Kamenev <yurkam447@gmail.com>
2023-11-23 12:22:59 +0300

    Added example with consuming answers one by one

--- klogic-utils/src/test/kotlin/org/klogic/utils/stopwatch/AnswerStopWatchTest.kt
+++ klogic-utils/src/test/kotlin/org/klogic/utils/stopwatch/AnswerStopWatchTest.kt
@@ -3,16 +3,23 @@ package org.klogic.utils.stopwatch
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.klogic.core.*
+import org.klogic.core.CustomTerm
+import org.klogic.core.State
 import org.klogic.core.Var.Companion.createTypedVar
+import org.klogic.core.conde
 import org.klogic.utils.listeners.AnswerStopWatch
 import org.klogic.utils.withEmptyContext
+import java.util.function.Consumer
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
+
+private const val sleepDurationMs: Long = 500
 
 class AnswerStopWatchTest {
     private object SleepingTerm : CustomTerm<SleepingTerm> {
         override val subtreesToUnify: Array<*>
             get() {
-                Thread.sleep(100)
+                Thread.sleep(sleepDurationMs)
                 return emptyArray<Unit>()
             }
 
@@ -22,7 +29,7 @@ class AnswerStopWatchTest {
     }
 
     @Test
-    fun testStopWatchOnPermutations() {
+    fun testStopWatchWithSleep() {
         withEmptyContext {
             val answerStopWatch = AnswerStopWatch()
 
@@ -38,7 +45,39 @@ class AnswerStopWatchTest {
 
             val durationMs = timings.single().second.inWholeMilliseconds
             println("Calculating answer took $durationMs ms")
-            assertTrue(durationMs >= 100)
+            assertTrue(durationMs >= sleepDurationMs)
+        }
+    }
+
+    @OptIn(ExperimentalTime::class)
+    private class PrintingStopWatch : Consumer<State>, Function1<State, Unit> {
+        private var timeMark = TimeSource.Monotonic.markNow()
+
+        override fun accept(state: State) {
+            val updatedTimeMark = TimeSource.Monotonic.markNow()
+            val elapsedTime = updatedTimeMark - timeMark
+            timeMark = updatedTimeMark
+
+            println("Got a new answer $state, elapsed time: ${elapsedTime.inWholeMilliseconds} ms")
+        }
+
+        override fun invoke(state: State) {
+            accept(state)
+        }
+    }
+
+    @Test
+    fun testAnswerOneByOne() {
+        withEmptyContext {
+            val printingStopWatch = PrintingStopWatch()
+
+            val size = 10
+            val term = (-1).createTypedVar<SleepingTerm>()
+            val goals = Array(size) { term `===` SleepingTerm }
+            val goalWithFewAnswers = conde(term `===` SleepingTerm, *goals)
+
+            val run = run(size + 1, term, goalWithFewAnswers, elementConsumer = printingStopWatch)
+            assertEquals(size + 1, run.size)
         }
     }
 }
