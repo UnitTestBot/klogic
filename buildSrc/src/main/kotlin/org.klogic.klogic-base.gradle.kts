0e37cb3a223bab74a3e9222da92f0d67e06181bd
DaniilStepanov <stepanov0995@mail.ru>
2023-12-08 16:36:45 +0300

    Set java version to 8

--- buildSrc/src/main/kotlin/org.klogic.klogic-base.gradle.kts
+++ buildSrc/src/main/kotlin/org.klogic.klogic-base.gradle.kts
@@ -19,15 +19,15 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += listOf("-Xjvm-default=all", "-Xcontext-receivers")
         allWarningsAsErrors = true
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
 
